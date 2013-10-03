adf56c1fa91ad789d3c67028025f6c576ccea9b3
xuyu <xuyu16@gmail.com>
2013-10-04 01:57:55 +0800

    add load testcase

--- ipfilter_test.go
+++ ipfilter_test.go
@@ -7,15 +7,26 @@ import (
 func TestIPFilter(t *testing.T) {
 	var f IPFilter
 	if err := f.AddIPNetString("192.168.1.0/24"); err != nil {
-		t.Fatalf("Add: 192.168.1.0/24, %s", err.Error())
+		t.Errorf("Add: 192.168.1.0/24, %s", err.Error())
 	}
 	if err := f.AddIPString("192.168.100.3"); err != nil {
-		t.Fatalf("Add: 192.168.100.3, %s", err.Error())
+		t.Errorf("Add: 192.168.100.3, %s", err.Error())
 	}
 	if !f.FilterIPString("192.168.1.3") {
-		t.Fatal("Filter Failed: 192.168.1.3")
+		t.Error("Filter Fail: 192.168.1.3")
 	}
 	if !f.FilterIPString("192.168.100.3") {
-		t.Fatal("Filter Failed: 192.168.100.3")
+		t.Error("Filter Fail: 192.168.100.3")
+	}
+}
+
+func TestLoad(t *testing.T) {
+	var f IPFilter
+	var data = []byte("192.168.1.0/24 192.168.1.3")
+	if err := f.Load(data); err != nil {
+		t.Error(err)
+	}
+	if !f.FilterIPString("192.168.1.3") {
+		t.Error("Filter Fail: 192.168.1.3")
 	}
 }
